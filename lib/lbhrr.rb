e573989a3544e5097b9a05ba0a38075e6c115ba8
Delaney Burke <delaney@vidtreone.com>
2023-12-08 14:52:26 +0200

    fix the drop

--- lib/lbhrr.rb
+++ lib/lbhrr.rb
@@ -12,25 +12,6 @@ module Lbhrr
 
   class SelfDelete
     def delete_current_directory
-      current_dir = Dir.pwd
-      parent_dir = File.dirname(current_dir)
-
-      # Requesting confirmation
-      puts "Are you sure you want to delete the directory: #{current_dir}? [y/N]"
-      response = gets.chomp.downcase
-      unless response == "y"
-        puts "Deletion aborted."
-        return
-      end
-
-      # Proceeding with deletion
-      puts "Changing directory to the parent directory..."
-      Dir.chdir(parent_dir)
-
-      puts "Deleting the directory: #{current_dir}"
-      FileUtils.rm_rf(current_dir)
-
-      puts "#{current_dir} has been deleted."
     end
   end
 
